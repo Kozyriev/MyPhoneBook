0deb702a335f47033f5140e5b88257b83340a569
Kozyriev <S.Kozurev@ukr.net>
2023-10-30 16:48:40 +0200

    registration all tests passed (hw 11)

--- src/test/java/manager/UserHelper.java
+++ src/test/java/manager/UserHelper.java
@@ -21,31 +21,36 @@ public class UserHelper extends BaseHelper {
     By btnLoginForm = xpath("//button[@name='login']");
     By textSuccessLoginPopUp = xpath("//a[@href='/contacts']");
 
-    By btnOpenRegForm = xpath("//a[@href='/login']");
     By inputEmailReg = xpath("//input[@name='email']");
     By inputPasswordReg = xpath("//input[@name='password']");
     By btnFormReg = xpath("//button[@name='registration']");
     By textPopUpSuccessRegH1 = xpath("//a[@href='/contacts']");
     By BtnAddForm = xpath("//a[@href='/add']");
-    By BtnSignOutForm = xpath("//button");
+    By BtnSignOutForm = xpath("//button[text()='Sign Out']");
+
+
+    public void openLoginPage(){
+
+        clickBase(btnLoginNavigatorMenu);
+    }
 
 
     public void login(UserDTO userDTO) {
-        clickBase(btnLoginNavigatorMenu);
+
         typeTextBase(inputEmailLoginForm, userDTO.getEmail());
         typeTextBase(inputPasswordLoginForm, userDTO.getPassword());
         clickBase(btnLoginForm);
     }
 
     public void login(UserDTOWith userDTO) {
-        clickBase(btnLoginNavigatorMenu);
+
         typeTextBase(inputEmailLoginForm, userDTO.getEmail());
         typeTextBase(inputPasswordLoginForm, userDTO.getPassword());
         clickBase(btnLoginForm);
     }
 
     public void loginUserDtoLombok(UserDtoLombok user) {
-        clickBase(btnLoginNavigatorMenu);
+
         typeTextBase(inputEmailLoginForm, user.getEmail());
         typeTextBase(inputPasswordLoginForm, user.getPassword());
         clickBase(btnLoginForm);
@@ -54,7 +59,7 @@ public class UserHelper extends BaseHelper {
 
     }
     public boolean validatePopUpMessageSuccessAfterLogin() {
-        clickByXY(BtnAddForm, 2, 2);
+//        clickByXY(BtnAddForm, 2, 2);
         return isTextEqual(textSuccessLoginPopUp, "CONTACTS");
 
     }
@@ -63,27 +68,15 @@ public class UserHelper extends BaseHelper {
         String expectedResult = "WRONG EMAIL OR PASSWORD";
         String actualResult = getTextAlert();
 
-        return isTextEqualGetToStrings(expectedResult, actualResult);
+        return isTextContainsGetToStrings(expectedResult, actualResult);
     }
 
     public boolean validateAlertMessageLoginIncorrectReg() {
 
-//        driver.findElement(By.xpath("//button[@onclick='jsConfirm()']")).click();
-//        Alert alerts2 = driver.switchTo().alert();
-//        System.out.println(alert2.getText());
-//        alerts2.dismiss();
-//        if(driver.getPageSource().contains("you clicked: Cancel"))
-//            System.out.println("you clicked: Cancel");
+        String expectedResult = "WRONG EMAIL OR PASSWORD FORMAT";
+        String actualResult = getTextAlert();
 
-        String expectedResult = "WRONG EMAIL OR PASSWORD FORMAT\n" +
-                "            EMAIL MUST CONTAINS ONE @ AND MINIMUM 2 SYMBOLS AFTER LAST DOT\n" +
-                "            PASSWORD MUST CONTAIN AT LEAST ONE UPPERCASE LETTER!\n" +
-                "            PASSWORD MUST CONTAIN AT LEAST ONE LOWERCASE LETTER!\n" +
-                "            PASSWORD MUST CONTAIN AT LEAST ONE DIGIT!\n" +
-                "            PASSWORD MUST CONTAIN AT LEAST ONE SPECIAL SYMBOL FROM [‘$’,’~’,’-‘,’_’]!".toUpperCase().trim();
-        String actualResult = getTextAlert().toUpperCase().trim();
-
-        return isTextEqualGetToStrings(expectedResult, actualResult);
+        return isTextContainsGetToStrings(expectedResult,actualResult);
     }
 
     public void fillRegistrationForm(UserDtoLombok user) {
@@ -96,15 +89,10 @@ public class UserHelper extends BaseHelper {
     }
 
     public boolean validatePopUpMessageSuccessAfterRegistration() {
-        clickByXY(BtnAddForm, 2, 2);
+//        clickByXY(BtnAddForm, 2, 2);
         String expectedResult = "CONTACTS".toUpperCase();
         return isTextEqual(textPopUpSuccessRegH1, expectedResult);
 
-    }
-
-    public boolean btnSignOutExist() {
-
-        return isElementExist(BtnSignOutForm);
     }
 
     public void SignOut() {
